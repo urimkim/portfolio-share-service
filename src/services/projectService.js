7b7e305adcacd4c95000108de79d1fb0cc540068
yurim <kinyou9894@naver.com>
2024-05-03 00:08:59 +0900

    require 수정

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,13 +1,10 @@
-// from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
-import { Project } from "../db";
-import { v4 as uuidv4 } from "uuid";
+const { Project } = require("../db");
+const { v4 : uuidv4 } = require("uuid");
 
 class ProjectService {
   static async addProject({ user_id, title, content }) {
-    // id로 유니크 값 사용
     const id = uuidv4();
 
-    // db에 저장
     const newProject = { id, user_id, title, content };
     const createdNewProject = await Project.create({ newProject });
 
@@ -15,7 +12,6 @@ class ProjectService {
   }
 
   static async getProject({ projectId }) {
-    // 해당 id를 가진 데이터가 db에 존재 여부 확인
     const project = await Project.findById({ projectId });
     if (!project) {
       const errorMessage =
@@ -34,7 +30,6 @@ class ProjectService {
   static async setProject({ projectId, toUpdate }) {
     let project = await Project.findById({ projectId });
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!project) {
       const errorMessage =
         "해당 id를 가진 프로젝트 데이터는 없습니다. 다시 한 번 확인해 주세요.";
@@ -59,7 +54,6 @@ class ProjectService {
   static async deleteProject({ projectId }) {
     const isDataDeleted = await Project.deleteById({ projectId });
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!isDataDeleted) {
       const errorMessage =
         "해당 id를 가진 프로젝트 데이터는 없습니다. 다시 한 번 확인해 주세요.";
@@ -70,4 +64,4 @@ class ProjectService {
   }
 }
 
-export { ProjectService };
+module.exports =  ProjectService;
