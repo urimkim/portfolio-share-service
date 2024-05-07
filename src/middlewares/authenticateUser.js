4c625d273c3bfb7a93d76a21ce799ae73e47ae05
yurim <kinyou9894@naver.com>
2024-05-07 20:45:29 +0900

    사용자 인증 토큰 시크릿키 추가

--- src/middlewares/authenticateUser.js
+++ src/middlewares/authenticateUser.js
@@ -1,25 +1,22 @@
 const jwt = require('jsonwebtoken');
+const secretKey = process.env.JWT; 
 
 function authenticateUser(req, res, next) {
-    const Token = req.headers["authorization"]?.split(" ")[1] ?? "null";
+    const token = req.headers["authorization"]?.split(" ")[1] ?? "null";
    
-    if (Token === "null") {
-        console.log(
-            "Authorization 토큰 없음",
-        );
-        res.status(400).send("로그인을 해주세요");
-        return;
+    if (token === "null") {
+        console.log("Authorization 토큰 없음");
+        return res.status(400).send("로그인을 해주세요");
     }
 
     try {
-        const jwtDecoded = jwt.verify(Token, secretKey);
+        const jwtDecoded = jwt.verify(token, secretKey);
         res.locals.user = jwtDecoded.userId;
-        
         next();
-      }  catch (error) {
-        res.status(400).json({ errorMessage: error.message }); 
-        return;
+    } catch (error) {
+        console.error("JWT 검증 오류:", error);
+        return res.status(400).json({ errorMessage: "유효하지 않은 토큰" });
     }
-};
+}
 
 module.exports = { authenticateUser };
