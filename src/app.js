f15fd8b60aaea41f95e54ed5642829de50803128
[AI10] 김영선 <ysus05@naver.com>
2024-05-07 14:46:24 +0900

    Fix:미들웨어 변수명 오류  수정

--- src/app.js
+++ src/app.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const { errorMiddleware } = require("./middlewares/errorMiddleware");
-const { awardRouter } = require("./routers/awardRouter");
-const { certificateRouter } = require("./routers/certificateRouter");
-const { projectRouter } = require("./routers/projectRouter");
-
 const cors = require("cors");
 const authRouter = require("./routers/authRouter");
 const userRouter = require("./routers/userRouter");
+const awardRouter = require("./routers/awardRouter");
+const certificateRouter = require("./routers/certificateRouter");
+const projectRouter = require("./routers/projectRouter");
+const errorMiddleware = require("./middlewares/errorMiddleware");
 const config = require("./config/index")
 
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(awardRouter);
-app.use(certificateRouter);
-app.use(projectRouter);
-app.use(errorMiddleware);
-app.use(cors());
+app.use(authenticationMiddleware);
 
 // router
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
+app.use("/api/my-info", awardRouter);
+app.use("/api/my-info", certificateRouter);
+app.use("/api/my-info", projectRouter);
+
+app.use(errorMiddleware);
 
 // server
 app.listen(config.port, () => {
